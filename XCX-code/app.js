60c9a1afd8790662702bb3fbf1820ae6eddafb16
xiaocc <cc13538560247@gmail.com>
2019-07-05 20:09:23 +0800

    获取豆瓣接口

--- XCX-code/app.js
+++ XCX-code/app.js
@@ -2,5 +2,6 @@ App({
   globalData:{
     g_isPlayingMusic:false,
     g_currentPostId:null,
+    doubanBase : "https://douban.uieee.com"
   }
 })
--- XCX-code/pages/movies/movies.js
+++ XCX-code/pages/movies/movies.js
@@ -1,20 +1,33 @@
+var app = getApp();
+
 Page({
 
 
   onLoad:function(event){
+
+    var inThreatersUrl = app.globalData.doubanBase+'/v2/movie/inTheaters';
+    var comingSoon = app.globalData.doubanBase +'/v2/movie/comingSoon';
+    var top250 = app.globalData.doubanBase +'v2/movie/top250';
+
+    this.getMovieDataList(inThreatersUrl);
+    this.getMovieDataList(comingSoon);
+    this.getMovieDataList(top250);
+
+   
+  },
+  getMovieDataList:function(url){
     wx.request({
-      url: 'https://api.douban.com/v2/movie/top250', //仅为示例，并非真实的接口地址
-      data: {
-        x: '',
-        y: ''
-      },
+      //https://api.douban.com/v2/movie/top250 会报错
+      url: url ,//仅为示例，并非真实的接口地址
+      method:"GET",
       header: {
-        'content-type': 'application/json' // 默认值
+        'content-type': 'application/xml' //   application/json 会报400,'https://douban.uieee.com/v2/movie/top250',
       },
       success(res) {
         console.log(res.data)
       }
     })
+
   }
   
 })
