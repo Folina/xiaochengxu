5d849a9356915006754015ecf7aba421ae4ada46
Folina <cc13538560247@gmail.com>
2019-07-06 00:02:37 +0800

    通过获取接口数据，做数据绑定，代替之前的假数据

--- XCX-code/pages/movies/movies.js
+++ XCX-code/pages/movies/movies.js
@@ -5,9 +5,9 @@ Page({
 
   onLoad:function(event){
 
-    var inThreatersUrl = app.globalData.doubanBase+'/v2/movie/inTheaters';
-    var comingSoon = app.globalData.doubanBase +'/v2/movie/comingSoon';
-    var top250 = app.globalData.doubanBase +'v2/movie/top250';
+    var inThreatersUrl = app.globalData.doubanBase+'/v2/movie/in_theaters';
+    var comingSoon = app.globalData.doubanBase +'/v2/movie/coming_soon';
+    var top250 = app.globalData.doubanBase +'/v2/movie/top250';
 
     this.getMovieDataList(inThreatersUrl);
     this.getMovieDataList(comingSoon);
@@ -16,6 +16,7 @@ Page({
    
   },
   getMovieDataList:function(url){
+    var that =this;
     wx.request({
       //https://api.douban.com/v2/movie/top250 会报错
       url: url ,//仅为示例，并非真实的接口地址
@@ -24,9 +25,35 @@ Page({
         'content-type': 'application/xml' //   application/json 会报400,'https://douban.uieee.com/v2/movie/top250',
       },
       success(res) {
+
         console.log(res.data)
+        that.processMovieData(res.data)
       }
     })
+
+  },
+
+  processMovieData:function(moivesData){
+    var movies =[];
+    for(var idx in moivesData.subjects){
+      var subject = moivesData.subjects[idx];
+      var title = subject.title
+      if(title.length>6){
+        title =title.substring(0,6)+"..."
+      };
+      var temp={
+        title:title,
+        average:subject.rating.average,
+        coverageUrl:subject.images.large,
+        movieId:subject.id
+      }
+      movies.push(temp)
+
+      this.setData({
+        movies:movies
+      })
+
+    }
 
   }
   
