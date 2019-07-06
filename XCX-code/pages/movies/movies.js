01a76761ab9901e21c330ea9ad8f536b978bbecc
Folina <cc13538560247@gmail.com>
2019-07-06 23:12:44 +0800

    实现调用对应接口，显示对应数据，主要用动态数据绑定,新增一个key的标识符settledKey，主要代码如下：readyData={},readyData[settledKey]={ movies:movies},this.setData(readyData)

--- XCX-code/pages/movies/movies.js
+++ XCX-code/pages/movies/movies.js
@@ -5,17 +5,18 @@ Page({
 
   onLoad:function(event){
 
-    var inThreatersUrl = app.globalData.doubanBase+'/v2/movie/in_theaters';
-    var comingSoon = app.globalData.doubanBase +'/v2/movie/coming_soon';
-    var top250 = app.globalData.doubanBase +'/v2/movie/top250';
+    var inThreatersUrl = app.globalData.doubanBase+'/v2/movie/in_theaters'+"?start=0&count=3";
+    var comingSoon = app.globalData.doubanBase + '/v2/movie/coming_soon'+"?start=0&count=3";
+    var top250 = app.globalData.doubanBase + '/v2/movie/top250' + "?start=0&count=3";
 
-    this.getMovieDataList(inThreatersUrl);
-    this.getMovieDataList(comingSoon);
-    this.getMovieDataList(top250);
+   //传入url,并且传入Key值，方便确定调用的是哪一个 
+    this.getMovieDataList(inThreatersUrl,"inThreater");
+    this.getMovieDataList(comingSoon,"comingSoon");
+    this.getMovieDataList(top250,"top250");
 
    
   },
-  getMovieDataList:function(url){
+  getMovieDataList: function (url, settledKey){
     var that =this;
     wx.request({
       //https://api.douban.com/v2/movie/top250 会报错
@@ -27,13 +28,13 @@ Page({
       success(res) {
 
         console.log(res.data)
-        that.processMovieData(res.data)
+        that.processMovieData(res.data,settledKey)
       }
     })
 
   },
 
-  processMovieData:function(moivesData){
+  processMovieData: function (moivesData, settledKey){
     var movies =[];
     for(var idx in moivesData.subjects){
       var subject = moivesData.subjects[idx];
@@ -49,9 +50,12 @@ Page({
       }
       movies.push(temp)
 
-      this.setData({
+      //用动态的数据绑定
+      var readyData={};
+      readyData[settledKey]={
         movies:movies
-      })
+      }
+      this.setData(readyData)
 
     }
 
