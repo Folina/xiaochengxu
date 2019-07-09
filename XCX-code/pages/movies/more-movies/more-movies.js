74572a6ee4ce34ad5a0c8d5ee2fcf1b7194bab5b
Folina <cc13538560247@gmail.com>
2019-07-09 23:44:37 +0800

    动态设置导航栏标题

--- XCX-code/pages/movies/more-movies/more-movies.js
+++ XCX-code/pages/movies/more-movies/more-movies.js
@@ -1,10 +1,19 @@
 // pages/movies/more-movies/more-movies.js
 Page({
   data: {
-
+    navigationBarTitle:""
   },
   onLoad: function (options) {
    var category = options.category;
     console.log(category);
+    this.data.navigationBarTitle = category
+  },
+
+  //如何动态设置导航栏标题
+  onReady:function(event){
+    wx.setNavigationBarTitle({
+      title: this.data.navigationBarTitle,
+    })
+
   }
 })
