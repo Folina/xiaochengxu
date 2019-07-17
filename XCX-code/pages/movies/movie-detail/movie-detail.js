8006c7d5f2292932c3923bf68552eb510984ee63
Folina <cc13538560247@gmail.com>
2019-07-17 23:55:33 +0800

    获取豆瓣详情数据

--- XCX-code/pages/movies/movie-detail/movie-detail.js
+++ XCX-code/pages/movies/movie-detail/movie-detail.js
@@ -1,4 +1,6 @@
-// pages/movies/movie-detail/movie-detail.js
+
+var util = require("../../utils/util.js")
+var app = getApp();
 Page({
 
   /**
@@ -12,55 +14,14 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var movieId = options.id;
+    // console.log(movieId)
+    var url = app.globalData.doubanBase +"/v2/movie/subject"+movieId;
+    util.http(url,this.processDoubanData);
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  processDoubanData:function(data){
+    console.log(data);
   }
+  
 })
